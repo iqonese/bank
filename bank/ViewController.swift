76c5825dbe53db87b79ff81186158954a83b3dd0
iqonese <iqonese@gmail.com>
2025-04-04 02:06:23 +0500

    Initial Commit

---
+++ bank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bank
+//
+//  Created by Said Tura Saidazimov on 04.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
